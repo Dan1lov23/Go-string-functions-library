59cc48e86d4359c68f213320b20aebe79c5d1009
kitik <svy1dan@gmail.com>
2026-04-09 17:29:06 +0300

    ndedadd functions diretcory and other minor changes

---
+++ strFunc/strFunc.go
@@ -0,0 +1,13 @@
+package strFunc
+
+func ReverseStr(str string) (reverseStr string) {
+
+	for index := len(str) - 1; index >= 0; index-- {
+		reverseStr += string(str[index])
+	} 
+
+	return reverseStr
+
+}
+
+
