e246e19bb15363bb1b1c1220dbbb9af67f6fefd8
kitik <svy1dan@gmail.com>
2026-04-10 22:14:47 +0300

    add other functions

--- strFunc/strFunc.go
+++ strFunc/strFunc.go
@@ -4,8 +4,53 @@ func ReverseStr(str string) (reverseStr string) {
 
 	for index := len(str) - 1; index >= 0; index-- {
 		reverseStr += string(str[index])
-	} 
+	}
 
-	return reverseStr
+	return
 
 }
+
+func AddElement(str string, position int, newElement string) (newStr string) {
+
+	for index := 0; index <= len(str); index++ {
+
+		if index == position {
+			newStr += newElement
+		}
+
+		if index <= len(str)-1 {
+			newStr += string(str[index])
+		}
+
+	}
+
+	return
+
+}
+
+func DeleteElement(str string, position int) (newStr string) {
+
+	for index := 0; index <= len(str) - 1; index++ {
+		if index != position {
+			newStr += string(str[index])
+		}
+	}
+
+	return
+
+}
+
+func ReplaceElement(str string, position int, element string) (newStr string) {
+
+	for index := 0; index <= len(str) - 1; index++ {
+		if index != position {
+			newStr += string(str[index])
+		} else {
+			newStr += element
+		}
+	}
+
+	return
+
+}
+
