d589e1bdc19604489354d7c91f686d80dffa187c
kitik <svy1dan@gmail.com>
2026-04-10 22:05:37 +0300

    add new functions

--- test/main.go
+++ test/main.go
@@ -7,5 +7,6 @@ import (
 
 func main() {
 	fmt.Println(strFunc.ReverseStr("golang"))
-	fmt.Println(strFunc.AddElementInString("lol", 3, "i"))
+	fmt.Println(strFunc.AddElementInString("golang", 3, "i"))
+	fmt.Println(strFunc.DeleteElementInString("golang", 2))
 }
